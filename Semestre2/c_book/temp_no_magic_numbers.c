71a2fd94926d86262f064f326e8ee8f2e57e353c
Flavia Bellesia <f.bellezia@gmail.com>
2026-08-13 16:18:38 +0100

    The C Book: until 1.4

--- Semestre2/c_book/temp_no_magic_numbers.c
+++ Semestre2/c_book/temp_no_magic_numbers.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 		0	//starting point in Fahrenheit
+#define HIGHER		300	//ending temp in Fahrenheit
+#define INCREASE	20	//temp increase each iteration
+
 /*
 	Function prints Fahrenheit(F)-Celsius(C) table
 	for fahr = 0, 20, ..., 300
@@ -8,24 +12,17 @@ int main()
 {
 	//Temperature variables
 	float fahr, cels;
-	//Fahr limit variables
-	int lower, higher, increase;
-	
-	lower = 0;		//start temperature
-	higher = 300;	//ending temperature
-	increase = 20;	//increase size
 
 	//Heading of the table
 	printf("Fahrenheit\tCelsius\n");
 
 	//Table
-	fahr = lower;
+	fahr = LOWER;
 
-	while (fahr <= higher)
+	while (fahr <= HIGHER)
 	{
 		cels = 5.0*(fahr-32.0)/9.0;
 		printf("%7.0f\t\t%6.1f\n", fahr, cels);
-		fahr += increase;
+		fahr += INCREASE;
 	}
 }
-
